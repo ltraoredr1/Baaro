25b004a6262ee86c8b1753e7fb28ffef7fc12f51
ltraoredr1 <ltraoredr1@gmail.com>
2026-09-21 18:57:44 +0000

    Delete api/stripe-redeem.js

--- api/stripe-redeem.js
+++
@@ -1,7 +0,0 @@
-import { applyCors } from './_shared.js';
-
-export default async function handler(req, res) {
-  if (applyCors(req, res)) return;
-  if (req.method === 'OPTIONS') return res.status(204).end();
-  return res.status(503).json({ ok: false, error: 'Le rachat cash n’est pas encore activé.' });
-}
