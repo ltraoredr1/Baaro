4477b974c116beb924c488fd03a37d23316f9a4e
ltraoredr1 <ltraoredr1@gmail.com>
2026-09-25 19:17:28 +0000

    Add files via upload

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,14 +1,10 @@
+/**
+ * Barrel lib BAARO — imports groupés
+ */
+export {
+  getCurrentUserId,
+  assertUserId,
+  isValidAuthUserId,
+} from "./currentUserId.js";
 
-export * from "./gifts.js";
-export * from "./liveRoles.js";
-export * from "./reportContent.js";
-export * from "./feedEvents.js";
-export * from "./notificationPreferences.js";
-export * from "./nativeCapabilities.js";
-export * from "./nativeBootstrap.js";
-export * from "./secureScreen.js";
-export * from "./nativePush.js";
-export * as webrtc from "./webrtc.js";
-export * as queries from "./queries.js";
-export * as walletApi from "./walletApi.js";
-export { getCurrentUserId, assertUserId, isValidAuthUserId } from "./currentUserId.js";
+export { supabase } from "../supabaseClient.js";
